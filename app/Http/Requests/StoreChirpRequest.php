69724d9edcf90f9607f66ada3167e7fcea5baa7b
devProMaleek <milikiadbay@gmail.com>
2023-02-01 18:54:57 +0100

    feat: Can Create chirp
    I made use of the store request class very well

---
+++ app/Http/Requests/StoreChirpRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreChirpRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        $user = $this->user();
+        return $user->id === auth()->id();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'message' => 'required|string|max:255',
+        ];
+    }
+}
---
+++ resources/js/Pages/Chirps/Index.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import InputError from "@/Components/InputError";
+import PrimaryButton from "@/Components/PrimaryButton";
+import { Head, useForm } from "@inertiajs/react";
+
+export default function Index({ chirps, auth }) {
+    const { data, setData, post, processing, reset, errors } = useForm({
+        message: "",
+    });
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        post(route("chirps.store"), { onSuccess: () => reset() });
+    };
+
+    return (
+        <AuthenticatedLayout auth={auth}>
+            <Head title="Chirps" />
+
+            <div className="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+                <form onSubmit={handleSubmit}>
+                    <textarea
+                        placeholder="What's on your mind?"
+                        className="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                        value={data.message}
+                        onChange={(event) =>
+                            setData("message", event.target.value)
+                        }
+                    />
+                    <InputError message={errors.message} className="mt-2" />
+                    <PrimaryButton className="mt-4" processing={processing}>
+                        Chirp
+                    </PrimaryButton>
+                </form>
+            </div>
+        </AuthenticatedLayout>
+    );
+}
